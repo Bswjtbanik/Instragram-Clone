9ebce63fb342dd77ee8c922b267d4e572e77f777
Biswajit <bswjtbanik@gmail.com>
2018-06-01 11:26:17 +0600

    Signup page

--- Instragram_clone/ViewController.swift
+++ Instragram_clone/ViewController.swift
@@ -9,17 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let plusphotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let emailTextfield : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    let usernameTextfield : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    let passwordTextfield : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let signUpbutton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor(red: 148/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(plusphotoButton)
+        plusphotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        plusphotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusphotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusphotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        view.addSubview(emailTextfield)
+        setupInputTextfield()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupInputTextfield(){
+        let stackView = UIStackView(arrangedSubviews: [emailTextfield , usernameTextfield , passwordTextfield , signUpbutton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: plusphotoButton.bottomAnchor, constant: 20),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+            stackView.heightAnchor.constraint(equalToConstant: 200)
+            ])
+
     }
-
-
 }
 
